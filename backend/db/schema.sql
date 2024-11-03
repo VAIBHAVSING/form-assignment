64801a4ed0e690e4027c4c12e5a297a43ab99392
Vaibhav Patil <vpatil5212@gmail.com>
2024-11-03 22:00:13 +0530

    Sql schema changed

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE forms (
   id SERIAL PRIMARY KEY,
   form_type VARCHAR(1) NOT NULL,
   name VARCHAR(100) NOT NULL,
-  country_code VARCHAR(10) NOT NULL,
+  country_code INT NOT NULL,
   phone_number VARCHAR(20) NOT NULL
 );
